b5ca90678a43a1613553146af48a5cbfdeb0f088
Chris Fernandez <cfernandezlinux@gmail.com>
2015-02-15 18:21:10 -0500

    more changes to my twitter ruby app

--- get_mytimeline-live.rb
+++ get_mytimeline-live.rb
@@ -9,6 +9,8 @@ require 'twitter'
 require 'rufus-scheduler'
 require 'colorize'
 
+topics = ["cybersecurity", "infosec", "cyberwar", "hacking"]
+
 @client = Twitter::Streaming::Client.new do |config|
   config.consumer_key = ENV[ 'TWITTER_CONSUMER_KEY' ]
   config.consumer_secret = ENV[ 'TWITTER_CONSUMER_SECRECT' ]
@@ -16,23 +18,6 @@ require 'colorize'
   config.access_token_secret = ENV[ 'TWITTER_OAUTH_TOKEN_SECRECT' ]
 end
 
-
-#  system "clear"
-#  @client.home_timeline.each do |t|
-#    puts "#{t.user.screen_name}:".red +  "#{t.text}".green
-#  end
-#end
-
-
-#scheduler = Rufus::Scheduler.new 
-#check()
-#scheduler.every '1m' do
-#  check()
-#end
-
-#scheduler.join
-
-topics = ["cybersecurity", "infosec", "cyberwar", "hacking"]
 @client.filter(track: topics.join(",")) do |object|
   puts "#{object.text}" if object.is_a?(Twitter::Tweet)
 end
